676da1ba9e73e772ba922bad4c6f989321ffa57c
Lucas Dearaujo <lucasaraujo45@outlook.com>
2025-03-13 15:14:05 -0400

    Initial Commit

---
+++ ApmCounter/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  ApmCounter
+//
+//  Created by Lucas De araujo on 3/13/25.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ ApmCounterTests/ApmCounterTests.swift
@@ -0,0 +1,17 @@
+//
+//  ApmCounterTests.swift
+//  ApmCounterTests
+//
+//  Created by Lucas De araujo on 3/13/25.
+//
+
+import Testing
+@testable import ApmCounter
+
+struct ApmCounterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
